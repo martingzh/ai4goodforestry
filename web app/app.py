43ad2f17660daf819393ef327e64ce541c7bfba5
Jiwon Lee <jiwon.lee@berkeley.edu>
2019-05-03 09:29:43 -0700

    had to tinker with directories

--- web app/app.py
+++ web app/app.py
@@ -10,17 +10,17 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'XYZ'
 app.config['MYSQL_DATABASE_DB'] = 'root@forestry'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/project/static/img/'
+# UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/project/static/img/'
 UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
  
-UPLOADED_IMAGES_DEST = TOP_LEVEL_DIR + '/project/static/img/'
+# UPLOADED_IMAGES_DEST = TOP_LEVEL_DIR + '/project/static/img/'
 UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
 
 mysql.init_app(app)
  
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/upload', methods= ['POST'])
 def upload():
@@ -36,7 +36,6 @@ def getall():
 	cur = mysql.connection.cursor()
 	cur.execute('''SELECT * FROM example''')
 	returnvals = cur.fetchall() #((1, "ID1"), (2, "ID2"),...)
-
 	printthis = ""
 	for i in returnvals:
 		printthis += i + "<br>"
